ca40f3989ddd1fac8789c3b8bea5f22f7dd5bad0
巫书轶 <wushuyi541@gmail.com>
2015-03-25 17:13:40 +0800

    fix jsHint

--- runtime.js
+++ runtime.js
@@ -1,71 +1,76 @@
 /**
  * Created by shuyi.wu on 2015/3/25.
  */
+/* global module */
 ;(function(global, factory){
-    if ( typeof module === "object" && typeof module.exports === "object" ) {
+    'use strict';
+
+    if ( typeof module === 'object' && typeof module.exports === 'object' ) {
         module.exports = factory();
-    }else if( typeof define === "function" && define.amd ){
+    }else if( typeof define === 'function' && define.amd ){
         define([], factory);
     }else {
         global.RunTime = factory();
     }
-})(typeof window !== "undefined" ? window : this, function() {
-    'use strict';
+})(
+    typeof window !== 'undefined' ? window : this,
+    function() {
+        'use strict';
 
-    function RunTime(time){
-        this._startDate = time ? new Date(time) : new Date();
-    }
-
-    RunTime.prototype._addNum = function(num, n){
-        var len = num.toString().length;
-        while(len < n) {
-            num = "0" + num;
-            len++;
+        function RunTime(time){
+            this._startDate = time ? new Date(time) : new Date();
         }
-        if(typeof num === "number"){
-            num.toString()
-        }
-        return num;
-    };
 
-    RunTime.prototype.getStartDate = function(){
-        return this._startDate;
-    };
+        RunTime.prototype._addNum = function(num, n){
+            var len = num.toString().length;
+            while(len < n) {
+                num = '0' + num;
+                len++;
+            }
+            if(typeof num === 'number'){
+                num.toString();
+            }
+            return num;
+        };
 
-    RunTime.prototype.getRunTime = function(){
-        return new Date() - this._startDate;
-    };
+        RunTime.prototype.getStartDate = function(){
+            return this._startDate;
+        };
 
-    RunTime.prototype.getRunTimeISO = function(){
-        return new Date(this.getRunTime()).toISOString();
-    };
+        RunTime.prototype.getRunTime = function(){
+            return new Date() - this._startDate;
+        };
 
-    RunTime.prototype.getRunTimeJson = function(type){
-        var time,jsonData ={}, split1, split2, split3, split4, split1end, split2end, split3end;
-        time = this.getRunTimeISO();
-        split1 =  time.split('-');
-        jsonData.year = split1[0] - 1970;
-        jsonData.mouth = split1[1] - 1;
-        split1end = split1[2];
-        split2 = split1end.split('T');
-        jsonData.day = split2[0] - 1;
-        split2end = split2[1];
-        split3 = split2end.split(':');
-        jsonData.hour = +split3[0];
-        jsonData.minute = +split3[1];
-        split3end = split3[2];
-        split4 = split3end.split('.');
-        jsonData.second = +split4[0];
-        if(type === 'string'){
-            jsonData.year = this._addNum(jsonData.year, 4);
-            jsonData.mouth = this._addNum(jsonData.mouth, 2);
-            jsonData.day = this._addNum(jsonData.day, 2);
-            jsonData.hour = this._addNum(jsonData.day, 2);
-            jsonData.minute = this._addNum(jsonData.minute, 2);
-            jsonData.second = this._addNum(jsonData.second, 2);
-        }
-        return jsonData;
-    };
+        RunTime.prototype.getRunTimeISO = function(){
+            return new Date(this.getRunTime()).toISOString();
+        };
 
-    return RunTime;
-});
+        RunTime.prototype.getRunTimeJson = function(type){
+            var time,jsonData ={}, split1, split2, split3, split4, split1end, split2end, split3end;
+            time = this.getRunTimeISO();
+            split1 =  time.split('-');
+            jsonData.year = split1[0] - 1970;
+            jsonData.mouth = split1[1] - 1;
+            split1end = split1[2];
+            split2 = split1end.split('T');
+            jsonData.day = split2[0] - 1;
+            split2end = split2[1];
+            split3 = split2end.split(':');
+            jsonData.hour = +split3[0];
+            jsonData.minute = +split3[1];
+            split3end = split3[2];
+            split4 = split3end.split('.');
+            jsonData.second = +split4[0];
+            if(type === 'string'){
+                jsonData.year = this._addNum(jsonData.year, 4);
+                jsonData.mouth = this._addNum(jsonData.mouth, 2);
+                jsonData.day = this._addNum(jsonData.day, 2);
+                jsonData.hour = this._addNum(jsonData.day, 2);
+                jsonData.minute = this._addNum(jsonData.minute, 2);
+                jsonData.second = this._addNum(jsonData.second, 2);
+            }
+            return jsonData;
+        };
+
+        return RunTime;
+    });
